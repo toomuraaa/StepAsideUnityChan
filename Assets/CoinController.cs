fa0743429be9e7b0428e12cee8d9f1fa61945faf
toomuraaa <t.oomuraaa@gmail.com>
2018-06-28 13:51:49 +0900

    StepAsideUnityChanプロジェクト
    課題提出

---
+++ Assets/CoinController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //回転を開始する角度を指定
+        this.transform.Rotate(0, Random.Range(0, 360), 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //回転をさせる
+        this.transform.Rotate(0, 3, 0);
+	}
+}
---
+++ Assets/DestroyItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyItem : MonoBehaviour {
+
+    private GameObject unitychan;
+
+    // Use this for initialization
+	void Start () {
+        //unitychanを取得
+        this.unitychan = GameObject.Find("unitychan");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //条件を満たせばオブジェクトを削除
+        if (this.transform.position.z <= unitychan.transform.position.z)
+        {
+            Destroy(gameObject,3.0f);
+        }
+	}
+}
